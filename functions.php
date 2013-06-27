d6b2515ba0f91f09fe03749c1dcfca3e28d10175
Dario Novoa <darionovoa@ideartte.com>
2013-06-27 15:38:45 -0700

    YCB footer and other un-branding…
    Removing all of 320press footprint except for credits since by the time
    we are done with this it will be a completely different animal.

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ require_once('library/shortcodes.php');
 // Custom Backend Footer
 add_filter('admin_footer_text', 'bones_custom_admin_footer');
 function bones_custom_admin_footer() {
-	echo '<span id="footer-thankyou">Developed by <a href="http://320press.com" target="_blank">320press</a></span>. Built using <a href="http://themble.com/bones" target="_blank">Bones</a>.';
+	echo '<span id="footer-thankyou">Developed by <a href="http://yourcustomblog.com" target="_blank">Your Custom Blog</a></span>.';
 }
 
 // adding it to the admin area
